8ead6495d980bd42f94ea09f9fecf3cb77cf4f8c
J Fern <jfern20@gmail.com>
2019-11-12 03:13:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,16 +3,15 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    num_1 = Kernel.rand 
-    num_2 = Kernel.rand 
-    num_3 = Kernel.rand
+    num_1 = Kernel.rand(1..2)
+    num_2 = Kernel.rand(1..2)
+    num_3 = Kernel.rand(1..2)
     
     resp.write "#{num_1}\n"
      resp.write "#{num_2}\n"
       resp.write "#{num_3}\n"
     
-    if num_1=="tic" && num_2 =="tac" num_3 == "toe" 
-      resp.write "You Win"
+  
     else     
       resp.write "You Lose"
     end 
